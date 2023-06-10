a0a37ff3f2de2f20904b70a9b73be197e7f3ea46
junjieyang <junjie.yang1@yahoo.com>
2023-06-10 22:39:46 +0800

    add new

--- a1_classes.py
+++ a1_classes.py
@@ -1,13 +1,15 @@
 """
 Name:Junjie Yang
 Date:07/06/2023
-Brief Project Description:
+Brief Project Description: Use class to re-draw the content of assessment1.
 GitHub URL:https://github.com/JCUS-CP1404/cp1404---travel-tracker---assignment-2-junjieyang-david
 """
 # Copy your first assignment to this file, then update it to use Place class
 # Optionally, you may also use PlaceCollection class
 
 import csv
+
+import places as places
 
 import place
 from place import Place
@@ -21,16 +23,16 @@ FILE_NAME = "places.csv"
 
 def main():
     print("Welcome to Travel Tracker 2.0-designed by Junjie Yang")
-    place_collection = PlaceCollection()
-    place_collection.load_places(FILE_NAME)
-    print(f"{len(place_collection.places)} places loaded from places.csv")
+    places = PlaceCollection()
+    places.load_places(FILE_NAME)
+    print(f"{len(places.places)} places loaded from places.csv")
     print(MENU)
     menu_choice = input(">>>").upper()
     while menu_choice != "Q":
         if menu_choice == "L":
             # list_places(places)
-            place_collection.sort("is_visited")
-            print(place_collection)
+            places.sort("is_visited")
+            print(places)
         elif menu_choice == "R":
             recommend_place(places)
         elif menu_choice == "A":
@@ -41,8 +43,8 @@ def main():
             print("Invalid menu choice")
         print(MENU)
         menu_choice = input(">>>").upper()
-    place_collection.save()
-    print(f"{len(place_collection.places)} places saved to {FILE_NAME}")
+    places.save()
+    print(f"{len(places.places)} places saved to {FILE_NAME}")
     print("Have a nice day :)")
 
 
@@ -63,75 +65,86 @@ def main():
 #         print("{0} places. You still want to visit {1} places.".format(counting, visited))
 
 
-def recommend_place(places):
-    unvisited_places = [place for place in places if place[3] == "n"]
-    if not unvisited_places:
-        print("No places left to visit!")
-        return
-    place = random.choice(unvisited_places)
-    print("Not sure where to visit next?")
-    print(f"How about... {place[0]} in {place[1]}?")
+# def recommend_place(places):
+#     unvisited_places = [place for place in places if place[3] == "n"]
+#     if not unvisited_places:
+#         print("No places left to visit!")
+#         return
+#     place = random.choice(unvisited_places)
+#     print("Not sure where to visit next?")
+#     print(f"How about... {place[0]} in {place[1]}?")
 
+def recommend_place(places):
+    """Get the top1 place order by the priority and visited status"""
+    p = random.choice(places.places)
+    print(p)
+    return
+
+
+# def add_place(places):
+#     name = input("Name: ")
+#     while name.strip() == " ":
+#         print("Input can not be blank")
+#         name = input("Name: ")
+#     country = input("Country: ")
+#     while country.strip() == " ":
+#         print("Input can not be blank")
+#         country = input("Country: ")
+#     priority = input("Priority: ")
+#     while not priority.isdigit() or int(priority) < 1:
+#         priority = input("Priority: ")
+#     print(f"{name} in {country}(priority {priority}) added to Travel Tracker")
+#     places.append([name, country, int(priority), "n"])
 
 def add_place(places):
     name = input("Name: ")
-    while name.strip() == " ":
-        print("Input can not be blank")
+    while len(name.strip()) == 0:
+        print("'Name' can not be blank")
         name = input("Name: ")
-    country = input("Country: ")
-    while country.strip() == " ":
-        print("Input can not be blank")
-        country = input("Country: ")
-    priority = input("Priority: ")
-    while not priority.isdigit() or int(priority) < 1:
-        priority = input("Priority: ")
-    print(f"{name} in {country}(priority {priority}) added to Travel Tracker")
-    places.append([name, country, int(priority), "n"])
 
+    country = input("Country: ")
+    while len(country.strip()) == 0:
+        print("'Country' can not be blank")
+        country = input("Country: ")
+
+    priority = input("Priority (Please enter an integer greater than 0) :")
+    while not priority.isdigit() or int(priority) < 1:
+        print("Please enter an interger greater than 0 represents the place's priority")
+        priority = input("Priority: ")
+
+    p=Place(name, country, int(priority), False)
+    places.add_place(p)
+    return
+def has_visited_place(places):
+    "check place has been visited"
+    for place in places:
+        if not place.visit():
+            return True
+        else:
+            return False
 
 def mark_visited(places):
-    unvisited_places = [place for place in places if place[3] == "n"]
-    if not unvisited_places:
-        print("No unvisited places")
-        return
-    list_places(places)
-    counter = 0
-    data_sorter = sorted(places, key=lambda row: (row[3], int(row[2])))
+    "Mark a user choose one place to visited"
+    places.sort("is_visited")
+    print(places)
     print("Enter the number of a place to mark as visited")
-
     while True:
         try:
             user_input = int(input(">>> "))
             if user_input <= 0:
                 print("Number must be > 0")
                 continue
-            elif user_input > len(places):
+            elif user_input > len(places.places):
                 print("Invalid place number")
                 continue
-            elif not check_place_mark(user_input, places):
+            elif not places.places[-1] == "n":
                 print("That Place is already visited")
             break
         except ValueError:
             print("Invalid Input; Enter a valid number")
 
-    for row in data_sorter:
-        counter = counter + 1
-        if counter == int(user_input):
-            if row[3] != "v":
-                print("{} in {} visited!".format(row[0], row[1]))
-            row[3] = "v"
 
 
-def check_place_mark(user_input, places):
-    data_sorter = sorted(places, key=lambda row: (row[3], int(row[2])))
-    count_place = 0
-    for row in data_sorter:
-        count_place += 1
-        if count_place == int(user_input):
-            if row[3] == "v":
-                return False
-            else:
-                return True
 
 
 main()
--- main.py
+++ main.py
@@ -1,18 +1,133 @@
 """
-Name:
-Date:
-Brief Project Description:
-GitHub URL:
+Name:Junjie Yang
+Date:07/06/2023
+Brief Project Description: Travel Tracker Program Assignment 2
+GitHub URL:https://github.com/JCUS-CP1404/cp1404---travel-tracker---assignment-2-junjieyang-david
 """
+
 # Create your main program in this file, using the TravelTrackerApp class
 
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
 
+from placecollection import PlaceCollection
+from place import Place
 
-class TravelTrackerApp(App):
+path='places.csv'
+visited_color = (1, 1, 1, 0.6)
+unvisited_color = (0, 1, 0.8, 0.7)
+
+class PlacesApp(App):
     """..."""
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.pc = PlaceCollection() # get a single PlaceCollection object
 
+    def build(self):
+        """Build Travel Tracker App """
+        self.title = "Travel Tracker"
+        self.root = Builder.load_file('app.kv')
+        try:
+            self.pc.load(path)
+            self.top_bar()
+            self.bottom_bar("Welcome to Travel Tracker")
+            self.places_list()
+        # Failed to find the file
+        except (FileNotFoundError, LookupError):
+            self.top_bar()
+            self.bottom_bar("Could not find places.csv!")
+        return self.root
+
+    def top_bar(self):
+        """Show the number of places still to visit"""
+        num = self.pc.unvisited_places()
+        self.root.ids.top_bar.text = "Places to visit: %s" % num
+        return
+
+    def bottom_bar(self,content):
+        """Show program messages"""
+        self.root.ids.bottom_bar.text = content
+        return
+
+    def places_list(self,key='is_visited'):
+        """Get sorted places list and create button for each of them"""
+        self.pc.sort(key)
+        self.root.ids.place_list.clear_widgets()  # clear widgets
+        for p in self.pc.places:
+            # set the text and color for each place
+            btn = Button(text=str(p),background_color= visited_color if p.is_visited else unvisited_color)
+            btn.bind(on_release=self.click)
+            btn.place = p # Store a reference to the place object in the button object
+            self.root.ids.place_list.add_widget(btn)
+
+    def click(self,btn):
+        """Change between visit & unvisited when user click the button"""
+        p = btn.place
+        # change button's text and color
+        if p.is_visited:
+            p.unvisited()
+            btn.text = str(p)
+            btn.background_color = unvisited_color
+        else:
+            p.visit()
+            btn.text = str(p)
+            btn.background_color = visited_color
+
+        # change the bottom bar content
+        if p.is_important() == 1:
+            if p.is_visited:
+                self.bottom_bar("You visited %s. Great travelling!" % p.name)
+            else:
+                self.bottom_bar("You need to visit %s. Get going!" % p.name)
+        else:
+            if p.is_visited:
+                self.bottom_bar("You visited %s." % p.name)
+            else:
+                self.bottom_bar("You need to visit %s." % p.name)
+
+        # change the top bar content
+        self.top_bar()
+        return
+
+    def add(self, name_input, country_input, priority_input):
+        """Handle Add New Book..."""
+        # error checking
+        name, country, priority = map(str.strip, (name_input.text, country_input.text, priority_input.text))
+        if not name and not country and not priority:
+            self.bottom_bar("All fields must be completed")
+            return
+        else:
+            # check if priority field a valid integer
+            try:
+                priority = int(priority)
+            except ValueError:
+                self.bottom_bar("Please enter a valid number")
+                return
+            # check if the input page is greater than 0
+            if priority < 1:
+                self.bottom_bar("Priority must be > 0")
+                return
+
+        p = Place(name, country, priority)
+        self.pc.add(p)
+        self.places_list()
+        self.top_bar()
+        self.bottom_bar("%s has been added to Travel Tracker." % name)
+        self.clear_input_field(name_input, country_input, priority_input)
+        return
+
+    @staticmethod
+    def clear_input_field(name_input, country_input, priority_input):
+        """Clear the user inputs fields"""
+        name_input.text = ''
+        country_input.text = ''
+        priority_input.text = ''
+
+    def on_stop(self):
+        """Save an updated places list file when the app is closed"""
+        self.pc.save(path)
+        return super().on_stop()
 
 if __name__ == '__main__':
-    TravelTrackerApp().run()
+    PlacesApp().run()
